838c1e02ff0858d16c367422a241f37c4d5c267e
Mayur Patel <pmayur0680@gmail.com>
2022-06-27 11:52:16 -0500

    fibonacci using tabulation

--- fibonacci.js
+++ fibonacci.js
@@ -5,11 +5,22 @@
 //    return fib(n - 1) + fib(n - 2);
 // }
 // optimize O(n) it by storing solutions of subproblems (dynamic programming), time complexity reduces to linear.
-function fib(n, memo=[]) {
+// memoization top down approach
+function fibMemoization(n, memo=[]) {
    if(n <= 2) return 1;
    if(memo[n] !== undefined) return memo[n];
    var res = fib(n-1, memo) + fib(n-2, memo);
    memo[n] = res;
    return res;
 }
-console.log(fib(100));
+// tabulation O(n) bottom up approach better than memoization compare to space complexity
+
+function fibTabulation(n) {
+   if(n <= 2) return 1;
+   var fibNums = [0, 1, 1];
+   for (let i = 3; i <= n; i++){
+      fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+   }
+   return fibNums[n];
+}
+console.log(fibTabulation(100));
